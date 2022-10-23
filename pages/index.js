f84344bdd6de149a599084c8d19ca803a9b867db
topDevfullstack <kalvinson961010@gmail.com>
2022-10-23 17:55:58 -0400

    Add upload-button

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
+import styles from '../styles/Home.module.css'
 
 const Home = ({ videos, error }) => {
   if (error) {
     return <div>An error occured: {error.message}</div>;
   }
   return (
-    <ul>
-      {videos.data.map(video => (
-        <li key={video.id}>{video.attributes.tilte}</li>
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <ul>
+        {videos.data.map(video => (
+          <li key={video.id}>{video.attributes.title}</li>
+        ))}
+      </ul>
+      <button className={styles.success}>Upload</button>
+    </div>
   );
 };
 
@@ -17,9 +21,10 @@ Home.getInitialProps = async ctx => {
   try {
     const res = await axios.get('http://localhost:1337/api/videos');
     const videos = res.data;
-    console.log(videos);
+    console.log('fdsa',videos);
     return { videos };
   } catch (error) {
+    console.log('error');
     return { error };
   }
 };
