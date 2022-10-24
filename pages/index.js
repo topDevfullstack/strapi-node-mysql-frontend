d63495bf3a1349b022406090cc69ee80122e77af
topDevfullstack <kalvinson961010@gmail.com>
2022-10-24 15:35:13 -0400

    Add ftp-scan-table

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,21 @@
+import React, { useState } from "react";
 import axios from 'axios';
 import SocialCard from "../components/Card";
 import styles from '../styles/Home.module.css'
-// import FTP from 'basic-ftp';
-
-// async function example() {
-  
-//   const client = new FTP.Client()
-//   client.ftp.verbose = true
-//   try {
-//       await client.access({
-//           host: "65.108.103.246",
-//           user: "freelancer",
-//           password: "6sEbmm4rj7tX2x72",
-//           secure: true
-//       })
-//       console.log(await client.list())
-//       // await client.uploadFrom("README.md", "README_FTP.md")
-//       // await client.downloadTo("README_COPY.md", "README_FTP.md")
-//   }
-//   catch(err) {
-//       console.log(err)
-//   }
-//   client.close()
-// }
 
 const Home = ({ videos, error }) => {
-  function handleClick () {
-    console.log('ftp');
-    // example();
+  const [list, setList] = useState({});
+
+  async function handleClick () {
+    try {
+      const res = await axios.get('http://localhost:1337/api/ftp-scan');
+      const videos = res.data;
+      console.log(videos);
+      setList(videos.data.attributes);
+    } catch (error) {
+      console.log('error');
+      return { error };
+    }
   }
 
   if (error) {
@@ -36,24 +24,28 @@ const Home = ({ videos, error }) => {
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        {/* <ul> */}
         <div className={styles.grid1}>
           {videos.data.map(video => (
-            // <li key={video.id}>
-              <SocialCard 
-                key={video.id}
-                video={video.attributes}
-                content='This is a media.'
-                likes='3'
-                // likeIsClicked={video.likeIsClicked}
-              />
-            // </li>
+            <SocialCard 
+              key={video.id}
+              video={video.attributes}
+              content='This is a media.'
+              likes='3'
+              // likeIsClicked={video.likeIsClicked}
+            />
           ))}
         </div>
-        {/* </ul> */}
         <div className={styles.grid1}>
           <button className={styles.success} onClick={handleClick}>Upload</button>
         </div>
+        {/* <table className={styles.grid1}>
+          {list && list.map(item => (
+            <tr key={item.id}>
+              <td>{item.title}</td>
+              <td>{item.provider}</td>
+            </tr>
+          ))}
+        </table> */}
       </div>
     </div>
   );
